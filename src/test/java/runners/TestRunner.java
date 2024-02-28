fbc539ac362d77212ca254617b1b543a4351874f
helil-prime <helil@primetechschool.com>
2024-02-27 21:08:08 -0500

    updated the click to action click

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -13,7 +13,7 @@ import io.cucumber.junit.CucumberOptions;
 				},
 		features="./src/test/resources/features",
 		glue="step_definitions",
-		tags="@smoke_tests"
+		tags="@regression"
 		)
 public class TestRunner {
 
